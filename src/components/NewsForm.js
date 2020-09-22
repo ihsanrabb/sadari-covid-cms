a27f2578ec9ef990b9826be34ba4ce3e1e0e15cb
ihsanrabb <ihsanrabbani25@gmail.com>
2020-09-22 21:22:48 +0700

    use firestore & add upload image

--- src/components/NewsForm.js
+++ src/components/NewsForm.js
@@ -1,30 +1,59 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
+import Grid from '@material-ui/core/Grid';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import {fb} from '../firebase'
+import LinearProgress from '@material-ui/core/LinearProgress'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: '2rem',
     '& > *': {
       margin: theme.spacing(1),
-      width: '50ch',
+      width: '100%',
     },
   },
   button: {
     margin: theme.spacing(1),
+  },
+  upload: {
+    width: '100%'
+  },
+  input: {
+    display: 'none'
   }
 }));
 
-const NewsForm = ({addOrEdit}) => {
+const NewsForm = (props) => {
   const classes = useStyles();
   const initialFieldValue = {
     title: "",
     description: "",
-    date: ""
+    date: "",
+    imageURL: ""
   }
   const [values, setValues] = useState(initialFieldValue)
+  const [selectedFile, setSelectedFile] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    if(props.currentId === '') {
+      setValues({
+        title: "",
+        description: "",
+        date: "",
+        imageURL: ""
+      })
+    } else {
+      setValues({
+        ...props.newsObj
+      })
+    }
+  }, [props.currentId, props.newsObj])
 
   const handleInputChange = e => {
     const {name, value} = e.target
@@ -36,7 +65,42 @@ const NewsForm = ({addOrEdit}) => {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    addOrEdit(values)
+    props.addOrEdit(values)
+    setSelectedFile('')
+    clearField()
+  }
+
+  const clearField = (e) => {
+    setValues({
+      title: "",
+      description: "",
+      date: "",
+      imageURL: ""
+    })
+    props.onClearField()
+  }
+
+  const onUpload = (event) => {
+    let file = event.target.files[0];
+    setLoading(true)
+    setSelectedFile(file.name)
+
+    let storageRef = fb.storage().ref('news/' + file.name + Date.now());
+    let uploadTask = storageRef.put(file);
+    uploadTask.on('state_changed', (snapshot) => {  
+    }, (error) => {
+      console.error(error)
+    },() => {
+      uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+        console.log('img', downloadURL)
+        setLoading(false)
+        setValues({
+          ...values,
+          imageURL : downloadURL
+        })
+      });
+    });
+
   }
 
   return (
@@ -46,7 +110,7 @@ const NewsForm = ({addOrEdit}) => {
         <TextField 
           label="Title" 
           variant="outlined"
-          value={values.title}  
+          value={values.title || ''}  
           name="title"
           onChange={handleInputChange}
         />
@@ -54,7 +118,7 @@ const NewsForm = ({addOrEdit}) => {
         <TextField 
           label="Tanggal" 
           variant="outlined"
-          value={values.date}  
+          value={values.date || ''}  
           name="date"
           onChange={handleInputChange}
         />
@@ -65,20 +129,79 @@ const NewsForm = ({addOrEdit}) => {
           multiline
           rows={4}
           variant="outlined"
-          value={values.description}
+          value={values.description || ''}
           name="description"
           onChange={handleInputChange}
         />
         <br/>
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.button}
-          endIcon={<SendIcon />}
-          type="submit"
-        >
-          Submit
-        </Button>
+
+        {loading 
+          ? <>
+              <LinearProgress /> 
+              <LinearProgress color="secondary" />
+            </>
+          : <>
+              <input
+                accept="image/*"
+                className={classes.input}
+                id="contained-button-file"
+                multiple
+                type="file"
+                onChange={onUpload}
+              />
+              <label htmlFor="contained-button-file">
+                <Button 
+                  variant="contained" 
+                  color="primary" 
+                  component="span" 
+                  fullWidth={true}
+                  startIcon={<CloudUploadIcon />}
+                >
+                  Upload
+                </Button>
+              </label>
+            </>
+        }
+
+        {values.imageURL &&
+          <img
+            width="100%"
+            className={classes.media}
+            src={values.imageURL}
+            alt="News"
+          />
+        }
+
+        <p>{selectedFile}</p>
+
+        <Grid container spacing={1}>
+          <Grid item xs={4}>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<DeleteIcon />}
+              type="button"
+              fullWidth={true}
+              onClick={() => clearField()}
+            >
+              Clear
+            </Button>
+          </Grid>
+          <Grid item xs={8}>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              endIcon={<SendIcon />}
+              type="submit"
+              fullWidth={true}
+            >
+              {props.currentId === '' ? 'Submit' : 'Update'}
+            </Button>
+          </Grid>
+        </Grid>
+
       </form>
     </>
   )
