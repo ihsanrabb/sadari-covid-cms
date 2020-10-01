61f4ad43745279c4ab14e771e805b26a59be7c80
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-01 16:50:06 +0700

    implement logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { fb } from '../firebase'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,17 +21,18 @@ const useStyles = makeStyles((theme) => ({
 const NavBar = () => {
   const classes = useStyles();
 
+  const handleLogout = () => {
+    fb.auth().signOut()
+  }
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
             News
           </Typography>
-          <Button color="inherit">Logout</Button>
+          <Button color="inherit" onClick={handleLogout}>Logout</Button>
         </Toolbar>
       </AppBar>
     </div>
