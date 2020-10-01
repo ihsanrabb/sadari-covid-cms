41f9f2803373433436ee5cac5cac15b5c5167fd8
ihsanrabb <ihsanrabbani25@gmail.com>
2020-10-01 16:48:16 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.css';
-import Container from '@material-ui/core/Container';
-import NavBar from './components/Navbar'
-import News from './components/News'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from '../src/pages/Home'
+import Login from '../src/pages/Login'
+import { AuthProvider } from '../src/AuthContext'
+import PrivateRoute from "./PrivateRoute"
 
 function App() {  
 
   return (
-    <div className="App">
-      <Container maxWidth={false}>
-        <NavBar />
-        <News />
-      </Container>
-    </div>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
